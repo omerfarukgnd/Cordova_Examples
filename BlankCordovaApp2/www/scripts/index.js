3d94cb88add9f40039d2501a78479038596e6da9
omerfarukgnd <33089487+omerfarukgnd@users.noreply.github.com>
2020-02-27 08:24:18 +0300

    1
    kkkkk

--- BlankCordovaApp2/www/scripts/index.js
+++ BlankCordovaApp2/www/scripts/index.js
@@ -1,30 +1,31 @@
-﻿// For an introduction to the Blank template, see the following documentation:
-// http://go.microsoft.com/fwlink/?LinkID=397704
-// To debug code on page load in cordova-simulate or on Android devices/emulators: launch your app, set breakpoints, 
-// and then run "window.location.reload()" in the JavaScript Console.
-(function () {
-    "use strict";
+﻿let app = {
+    init: function () {
+        document.getElementById('btn').addEventListener('click', app.takephoto);
+    },
+    takephoto: function () {
+        let opts = {
+            quality: 80,
+            destinationType: Camera.DestinationType.FILE_URI,
+            sourceType: Camera.PictureSourceType.CAMERA,
+            mediaType: Camera.MediaType.PICTURE,
+            encodingType: Camera.EncodingType.JPEG,
+            cameraDirection: Camera.Direction.BACK,
+            targetWidth: 300,
+            targetHeight: 400
+        };
 
-    document.addEventListener( 'deviceready', onDeviceReady.bind( this ), false );
+        navigator.camera.getPicture(app.ftw, app.wtf, opts);
+    },
+    ftw: function (imgURI) {
+        document.getElementById('photo').src = imgURI;
+        var image = document.getElementById("photo");
+        image.setAttribute('style', 'transform:rotate(270deg)');
+        image.setAttribute('style', 'width:100%');
 
-    function onDeviceReady() {
-        // Handle the Cordova pause and resume events
-        document.addEventListener( 'pause', onPause.bind( this ), false );
-        document.addEventListener( 'resume', onResume.bind( this ), false );
-        
-        // TODO: Cordova has been loaded. Perform any initialization that requires Cordova here.
-        var parentElement = document.getElementById('deviceready');
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
-    };
+    },
+    wtf: function (msg) {
+        document.getElementById('msg').textContent = msg;
+    }
+};
 
-    function onPause() {
-        // TODO: This application has been suspended. Save application state here.
-    };
-
-    function onResume() {
-        // TODO: This application has been reactivated. Restore application state here.
-    };
-} )();
+document.addEventListener('deviceready', app.init);
